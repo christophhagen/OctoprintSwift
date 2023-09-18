77e43d1d997c889a99052ca0c58af89a68de7f17
Christoph Hagen <github@christophhagen.de>
2023-09-19 00:46:12 +0200

    Add job state

--- Sources/OctoprintSwift/JobStatus/JobStatus.swift
+++ Sources/OctoprintSwift/JobStatus/JobStatus.swift
@@ -14,7 +14,7 @@ struct JobStatus: Codable, Equatable {
     /**
      A textual representation of the current state of the job or connection, e.g. “Operational”, “Printing”, “Pausing”, “Paused”, “Cancelling”, “Error”, “Offline”, “Offline after error”, “Opening serial connection”, … – please note that this list is not exhaustive!
      */
-    let state: String
+    let state: JobState
 
     /// Any error message for the job or connection, only set if there has been an error.
     let error: String?
@@ -29,4 +29,58 @@ struct EmptyJSON: Codable, Equatable {
     func encode(to encoder: Encoder) throws {
 
     }
+}
+
+enum JobState {
+    case operational
+    case printing
+    case pausing
+    case paused
+    case cancelling
+    case error
+    case offline
+    case offlineAfterError
+    case openingSerialConnection
+    case unknown(String)
+}
+
+extension JobState: RawRepresentable {
+
+    init(rawValue: String) {
+        switch rawValue {
+        case "Operational": self = .operational
+        case "Printing": self = .printing
+        case "Pausing": self = .pausing
+        case "Paused": self = .paused
+        case "Cancelling": self = .cancelling
+        case "Error": self = .error
+        case "Offline": self = .offline
+        case "Offline after error": self = .offlineAfterError
+        case "Opening serial connection": self = .openingSerialConnection
+        default: self = .unknown(rawValue)
+        }
+    }
+
+    var rawValue: String {
+        switch self {
+        case .operational: return "Operational"
+        case .printing: return "Printing"
+        case .pausing: return "Pausing"
+        case .paused: return "Paused"
+        case .cancelling: return "Cancelling"
+        case .error: return "Error"
+        case .offline: return "Offline"
+        case .offlineAfterError: return "Offline after error"
+        case .openingSerialConnection: return "Opening serial connection"
+        case .unknown(let string): return string
+        }
+    }
+}
+
+extension JobState: Codable {
+
+}
+
+extension JobState: Equatable {
+
 }
