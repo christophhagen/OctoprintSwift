60fc0a620ae11d4863d4c4752a7d4a376851382d
Christoph Hagen <github@christophhagen.de>
2023-09-19 01:23:43 +0200

    Attempt to fix server info decoding

--- Sources/OctoprintSwift/Data Model/ServerInformation.swift
+++ Sources/OctoprintSwift/Data Model/ServerInformation.swift
@@ -13,21 +13,7 @@ public struct ServerInformation {
     }
 }
 
-extension ServerInformation: Decodable {
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.version = try container.decode(Version.self, forKey: .version)
-        do {
-            let isInSafeMode = try container.decode(Bool.self, forKey: .safeMode)
-            guard !isInSafeMode else {
-                throw DecodingError.dataCorrupted(.init(codingPath: [CodingKeys.safeMode], debugDescription: "Found boolean, but value was not false"))
-            }
-            self.safeMode = nil
-        } catch {
-            self.safeMode = try container.decode(SafeMode.self, forKey: .safeMode)
-        }
-    }
+extension ServerInformation: Codable {
 
     enum CodingKeys: String, CodingKey {
         case version = "version"
