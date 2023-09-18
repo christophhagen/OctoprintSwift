b4ceb19cb7493242650ee8a360c3bf7ec837151b
Christoph Hagen <github@christophhagen.de>
2023-09-19 01:02:20 +0200

    Test linux networking

---
+++ Sources/OctoprintSwift/URLSession+Extensions.swift
@@ -0,0 +1,27 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+
+extension URLSession {
+
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        let result: Result<(response: URLResponse, data: Data), Error> = await withCheckedContinuation { continuation in
+            let task = dataTask(with: request) { data, response, error in
+                if let error {
+                    continuation.resume(returning: .failure(error))
+                } else {
+                    continuation.resume(returning: .success((response!, data!)))
+                }
+            }
+            task.resume()
+        }
+        switch result {
+        case .failure(let error):
+            throw error
+        case .success(let result):
+            return (result.data, result.response)
+        }
+    }
+}
+
+#endif
