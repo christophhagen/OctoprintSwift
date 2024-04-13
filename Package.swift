16765cce511e5f8d8c0aa7f45df2b1e1a321b0a2
Christoph Hagen <github@christophhagen.de>
2024-04-13 11:08:57 +0200

    Add DocC for documentation

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ let package = Package(
     name: "OctoprintSwift",
     platforms: [.macOS(.v11), .iOS(.v13)],
     products: [.library(name: "OctoprintSwift", targets: ["OctoprintSwift"])],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "OctoprintSwift",
